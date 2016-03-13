432a4a09f659ce06ba88f9bf9d62bf07911fa480
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-12 21:47:49 -0800

    config.js containing Yelp API credentials added

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+	consumer_key: 'QfJXdX9KxsuHZ04ruLg3qg',
+	consumer_secret: 'E5kGeX124hNOOgywvXkPxo39DPE',
+  	token: 'ixaAxWq6YszaeLPwYYf_rECBf_fPdcjC',
+  	token_secret: 'jtklLDOecbZuR33MYHglnvPHix8'
+
+};
