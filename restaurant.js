4371a5f854e7f12801b8f3bb91753b58f9729e88
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-12 23:39:54 -0800

    restaurant class added

---
+++ restaurant.js
@@ -0,0 +1,11 @@
+function Restaurant(name, rating, url, phone, image_url, display_address, coordiate) {  
+	this.name = name;
+	this.rating = rating;
+	this.url = url;
+	this.phone = phone;
+	this.image_url = image_url;
+	this.display_address = display_address;
+	this.coordiate = coordiate;
+}
+
+module.exports = Restaurant;
