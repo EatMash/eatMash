f042bff85203250af21a1bcf2df45b9a84e06e6a
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-14 21:35:14 -0700

    use yelp_api

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var yelp_api = require('./yelp_api');
+
 var app = express();
-var config = require('./config');
-var Restaurant = require('./restaurant');
-var _ = require('underscore');
-var Yelp = require('yelp');
-var yelp = new Yelp({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  token: config.token,
-  token_secret: config.token_secret
-});
+app.use(bodyParser());
 
 app.get('/', function (req, res) {
   res.send("Yelp Hackathon Backend Running...");
   res.end();
 });
+
+app.post('/test', function (req, res) {
+
+	var term = req.body.term;
+	var cll = req.body.cll;
+	var minimum_rating = req.body.minrat;
+	var accepted = req.body.accepted;
+	var not_accepted = req.body.not_accepted;
+	var num = 1;
+
+	if (minimum_rating == undefined) minimum_rating = 0.0;
+  	if (isNaN(Number(minimum_rating)) || Number(minimum_rating) < 0 || Number(minimum_rating) > 5.0) {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify("{\"statusCode\":400,\"data\":\"{\"error\": {\"text\": \"minimum rating should be a number from 0.0 to 5.0\", \"id\": \"INVALID_PARAMETER\", \"field\": \"minrat\"}}\"}"));
+		return;
+	}
+
+});
+
+
 
 app.get('/api', function (req, res) { // name, rating, url, phone, image_url, display_address, coordinate
 	
@@ -31,56 +45,58 @@ app.get('/api', function (req, res) { // name, rating, url, phone, image_url, di
 		return;
 	}
 
-	var candidates = [];	
-	var returnValues = [];
+	yelp_api.call(term, cll, minimum_rating, num, res);
 
-	var queryObject = {};
-	if (term != undefined) queryObject.term = term;
-	queryObject.location = 'location';
-	queryObject.cll = cll;
+	// var candidates = [];	
+	// var returnValues = [];
 
-	yelp.search(queryObject)
-	.then(function (data) {
+	// var queryObject = {};
+	// if (term != undefined) queryObject.term = term;
+	// queryObject.location = 'location';
+	// queryObject.cll = cll;
 
-		for (business of data.businesses) {
+	// yelp.search(queryObject)
+	// .then(function (data) {
 
-			if (business.rating >= minimum_rating) { 
+	// 	for (business of data.businesses) {
 
-				var newRestaurant = new Restaurant(
-					business.name, 
-					business.rating,
-					business.url,
-					business.phone,
-					business.image_url,
-					business.location.display_address,
-					business.location.coordinate
-				);
+	// 		if (business.rating >= minimum_rating) { 
 
-				candidates.push(newRestaurant);
-			}
-		}
+	// 			var newRestaurant = new Restaurant(
+	// 				business.name, 
+	// 				business.rating,
+	// 				business.url,
+	// 				business.phone,
+	// 				business.image_url,
+	// 				business.location.display_address,
+	// 				business.location.coordinate
+	// 			);
 
-		if (!_.isEmpty(candidates)) {
-			var random_index = _.random(0, _.size(candidates) - 1);
-			returnValue = candidates[random_index];
-		}
+	// 			candidates.push(newRestaurant);
+	// 		}
+	// 	}
 
-		while(num > 0 && !_.isEmpty(candidates)) {
-			var random_index = _.random(0, _.size(candidates) - 1);
-			returnValues.push(candidates[random_index]);
-			candidates[random_index] = null;
-			num--;
-			candidates = _.compact(candidates);
-		}
+	// 	if (!_.isEmpty(candidates)) {
+	// 		var random_index = _.random(0, _.size(candidates) - 1);
+	// 		returnValue = candidates[random_index];
+	// 	}
+
+	// 	while(num > 0 && !_.isEmpty(candidates)) {
+	// 		var random_index = _.random(0, _.size(candidates) - 1);
+	// 		returnValues.push(candidates[random_index]);
+	// 		candidates[random_index] = null;
+	// 		num--;
+	// 		candidates = _.compact(candidates);
+	// 	}
 		
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify(returnValues));
-	})
-	.catch(function (err) {
-		console.error(err);
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify(err));
-	});
+	// 	res.setHeader('Content-Type', 'application/json');
+	// 	res.send(JSON.stringify(returnValues));
+	// })
+	// .catch(function (err) {
+	// 	console.error(err);
+	// 	res.setHeader('Content-Type', 'application/json');
+	// 	res.send(JSON.stringify(err));
+	// });
 
 });
 
