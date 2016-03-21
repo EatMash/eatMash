479ff804195e415406a2e1d6fb76244e52489a96
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-20 23:53:40 -0700

    database.js modified

--- database/database.js
+++ database/database.js
@@ -3,5 +3,5 @@ var config = require('../config');
 var client = new pg.Client(config.connectionString);
 
 client.connect();
-var query = client.query('CREATE TABLE mashups(id SERIAL PRIMARY KEY, name VARCHAR(1000), uuid VARCHAR(1000),rating real,url VARCHAR(1000),phone VARCHAR(100),image_url VARCHAR(1000),display_address VARCHAR(1000),latitude bigint, longitude bigint, timestamp timestamp default current_timestamp)');
+var query = client.query('CREATE TABLE mashups(id SERIAL PRIMARY KEY, name VARCHAR(1000), uuid VARCHAR(1000),rating real,url VARCHAR(1000),phone VARCHAR(100),image_url VARCHAR(1000),display_address VARCHAR(1000), latitude VARCHAR(1000), longitude VARCHAR(1000), timestamp timestamp default current_timestamp)');
 query.on('end', function() { client.end(); });
