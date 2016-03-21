2246835fe4f83d74692046167c6678adb9825bef
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-21 00:14:40 -0700

    yelp_api js format applied

--- yelp_api.js
+++ yelp_api.js
@@ -4,17 +4,17 @@ var Restaurant = require('./restaurant');
 var _ = require('underscore');
 var Yelp = require('yelp');
 var yelp = new Yelp({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  token: config.token,
-  token_secret: config.token_secret
+	consumer_key: config.consumer_key,
+	consumer_secret: config.consumer_secret,
+	token: config.token,
+	token_secret: config.token_secret
 });
 
 module.exports = {
 
 	call: function(term, location, minimum_rating, num, res) {
 
-    	var candidates = [];	
+		var candidates = [];
 		var returnValues = [];
 
 		var queryObject = {};
@@ -22,63 +22,63 @@ module.exports = {
 		queryObject.location = location;
 
 		yelp.search(queryObject)
-		.then(function (data) {
+			.then(function(data) {
 
-			var total_number = data.total;
-			var offset = null;
-			if (total_number <= 100) offset = 0;
-			else offset = _.random(0, 80);
+				var total_number = data.total;
+				var offset = null;
+				if (total_number <= 100) offset = 0;
+				else offset = _.random(0, 80);
 
-			queryObject.offset = offset;
+				queryObject.offset = offset;
 
-			yelp.search(queryObject)
-			.then(function (data) {
+				yelp.search(queryObject)
+					.then(function(data) {
 
-				for (business of data.businesses) {
+						for (business of data.businesses) {
 
-					if (business.rating >= minimum_rating) { 
+							if (business.rating >= minimum_rating) {
 
-						var newRestaurant = new Restaurant(
-							business.name, 
-							business.rating,
-							business.url,
-							business.phone,
-							business.image_url,
-							business.location.display_address,
-							business.location.coordinate
-						);
+								var newRestaurant = new Restaurant(
+									business.name,
+									business.rating,
+									business.url,
+									business.phone,
+									business.image_url,
+									business.location.display_address,
+									business.location.coordinate
+								);
 
-						candidates.push(newRestaurant);
-					}
-				}
+								candidates.push(newRestaurant);
+							}
+						}
 
-				if (!_.isEmpty(candidates)) {
-					var random_index = _.random(0, _.size(candidates) - 1);
-					returnValue = candidates[random_index];
-				}
+						if (!_.isEmpty(candidates)) {
+							var random_index = _.random(0, _.size(candidates) - 1);
+							returnValue = candidates[random_index];
+						}
 
-				while(num > 0 && !_.isEmpty(candidates)) {
-					var random_index = _.random(0, _.size(candidates) - 1);
-					returnValues.push(candidates[random_index]);
-					candidates[random_index] = null;
-					num--;
-					candidates = _.compact(candidates);
-				}
-				
-				res.setHeader('Content-Type', 'application/json');
-				res.send(JSON.stringify(returnValues));
+						while (num > 0 && !_.isEmpty(candidates)) {
+							var random_index = _.random(0, _.size(candidates) - 1);
+							returnValues.push(candidates[random_index]);
+							candidates[random_index] = null;
+							num--;
+							candidates = _.compact(candidates);
+						}
+
+						res.setHeader('Content-Type', 'application/json');
+						res.send(JSON.stringify(returnValues));
+					})
+					.catch(function(err) {
+						console.error(err);
+						res.setHeader('Content-Type', 'application/json');
+						res.send(JSON.stringify(err));
+					});
+
 			})
-			.catch(function (err) {
+			.catch(function(err) {
 				console.error(err);
 				res.setHeader('Content-Type', 'application/json');
 				res.send(JSON.stringify(err));
 			});
-
-		})
-		.catch(function (err) {
-			console.error(err);
-			res.setHeader('Content-Type', 'application/json');
-			res.send(JSON.stringify(err));
-		});
-  	}
+	}
 };
