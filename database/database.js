edf8d3140e8b7afa20270291ee4928f7075979da
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-20 23:06:57 -0700

    in database: coordiate -> latitude + longitude

--- database/database.js
+++ database/database.js
@@ -3,5 +3,5 @@ var config = require('../config');
 var client = new pg.Client(config.connectionString);
 
 client.connect();
-var query = client.query('CREATE TABLE mashups(id SERIAL PRIMARY KEY, name VARCHAR(40), uuid VARCHAR(40),rating real,url VARCHAR(40),phone VARCHAR(40),image_url VARCHAR(40),display_address VARCHAR(40),coordiate VARCHAR(40),timestamp timestamp default current_timestamp)');
+var query = client.query('CREATE TABLE mashups(id SERIAL PRIMARY KEY, name VARCHAR(1000), uuid VARCHAR(1000),rating real,url VARCHAR(1000),phone VARCHAR(100),image_url VARCHAR(1000),display_address VARCHAR(1000),latitude bigint, longitude bigint, timestamp timestamp default current_timestamp)');
 query.on('end', function() { client.end(); });
