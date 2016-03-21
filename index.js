e4077d284c6533f879b534f731bb44d7e57f39b8
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-20 21:55:09 -0700

    test commented out

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ app.get('/', function (req, res) {
   res.end();
 });
 
-app.post('/test', function (req, res) {
+// app.post('/test', function (req, res) {
 
-	var term = req.body.term;
-	var cll = req.body.cll;
-	var minimum_rating = req.body.minrat;
-	var accepted = req.body.accepted;
-	var not_accepted = req.body.not_accepted;
-	var num = 1;
+// 	var term = req.body.term;
+// 	var cll = req.body.cll;
+// 	var minimum_rating = req.body.minrat;
+// 	var accepted = req.body.accepted;
+// 	var not_accepted = req.body.not_accepted;
+// 	var num = 1;
 
-	if (minimum_rating == undefined) minimum_rating = 0.0;
-  	if (isNaN(Number(minimum_rating)) || Number(minimum_rating) < 0 || Number(minimum_rating) > 5.0) {
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify("{\"statusCode\":400,\"data\":\"{\"error\": {\"text\": \"minimum rating should be a number from 0.0 to 5.0\", \"id\": \"INVALID_PARAMETER\", \"field\": \"minrat\"}}\"}"));
-		return;
-	}
+// 	if (minimum_rating == undefined) minimum_rating = 0.0;
+//   	if (isNaN(Number(minimum_rating)) || Number(minimum_rating) < 0 || Number(minimum_rating) > 5.0) {
+// 		res.setHeader('Content-Type', 'application/json');
+// 		res.send(JSON.stringify("{\"statusCode\":400,\"data\":\"{\"error\": {\"text\": \"minimum rating should be a number from 0.0 to 5.0\", \"id\": \"INVALID_PARAMETER\", \"field\": \"minrat\"}}\"}"));
+// 		return;
+// 	}
 
-});
+// });
 
 app.get('/api', function (req, res) { // name, rating, url, phone, image_url, display_address, coordinate
 	
