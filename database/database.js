4f8da6be4bff7b74a362dd4589254bed2bb2f8b7
Jaeyoon Jung <jaeyoonjung85@gmail.com>
2016-03-20 22:54:52 -0700

    database timestamp added

--- database/database.js
+++ database/database.js
@@ -3,5 +3,5 @@ var config = require('../config');
 var client = new pg.Client(config.connectionString);
 
 client.connect();
-var query = client.query('CREATE TABLE mashups(id SERIAL PRIMARY KEY, name VARCHAR(40), uuid VARCHAR(40),rating real,url VARCHAR(40),phone VARCHAR(40),image_url VARCHAR(40),display_address VARCHAR(40),coordiate VARCHAR(40))');
+var query = client.query('CREATE TABLE mashups(id SERIAL PRIMARY KEY, name VARCHAR(40), uuid VARCHAR(40),rating real,url VARCHAR(40),phone VARCHAR(40),image_url VARCHAR(40),display_address VARCHAR(40),coordiate VARCHAR(40),timestamp timestamp default current_timestamp)');
 query.on('end', function() { client.end(); });
