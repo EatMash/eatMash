4f3768efa714337496de1a1130ff06a7a4b25d2a
IzumiSy <beetle-noise@gmx.com>
2016-03-22 15:27:13 -0700

    Implement meal type specifications

--- yelp_api.js
+++ yelp_api.js
@@ -3,6 +3,7 @@ var pg = require('pg');
 var Restaurant = require('./restaurant');
 var _ = require('underscore');
 var Yelp = require('yelp');
+var Bucks = require('bucks');
 var yelp = new Yelp({
 	consumer_key: config.consumer_key,
 	consumer_secret: config.consumer_secret,
@@ -10,7 +11,96 @@ var yelp = new Yelp({
 	token_secret: config.token_secret
 });
 
+// Filter restaurants
+var filter_restaurants = function(data, minrat) {
+  var candidates = [];
+
+  for (business of data.businesses) {
+    if (business.rating >= minrat) {
+      var newRestaurant = new Restaurant(
+        business.name,
+        business.rating,
+        business.url,
+        business.phone,
+        business.image_url,
+        business.location.display_address,
+        business.location.coordinate
+      );
+      candidates.push(newRestaurant);
+    }
+  }
+
+  return candidates;
+};
+
+//
+// [Object args]
+// - query_object: object
+//  - breakfast: string
+//  - lunch: string
+//  - dinner: string
+// - location: string
+//
+// [How this work]
+// Call Yelp API asynchonizely to get restaurants that serve breakfast, lunch, and dinner.
+// If fetching completes successfully, this function randomly selects each restaurant for every meals.
+//
+var call_yelp_api = function(query_object, location, minrat, callback) {
+  var breakfast_query = query_object.breakfast || "";
+  var lunch_query = query_object.lunch || "";
+  var dinner_query = query_object.dinner || "";
+
+  var randomly_return = function(data) {
+    var restaurants = filter_restaurants(data, minrat);
+    return restaurants[_.random(0, _.size(restaurants) - 1)];
+  };
+
+  var meals_obtainer = {
+    breakfast: function(err, res, next) {
+      var query = {
+        term: "breakfast " + breakfast_query,
+        location: location
+      };
+      yelp.search(query).then(function(data) {
+        return next(null, randomly_return(data));
+      }).catch(function(err) {
+        throw new Error("Yelp API error: breakfast");
+      });
+    },
+    lunch: function(err, res, next) {
+      var query = {
+        term: "lunch " + lunch_query,
+        location: location
+      }
+      yelp.search(query).then(function(data) {
+        return next(null, randomly_return(data));
+      }).catch(function(err) {
+        throw new Error("Yelp API error: lunch");
+      });
+    },
+    dinner: function(err, res, next) {
+      var query = {
+        term: "dinner " + dinner_query,
+        location: location
+      }
+      yelp.search(query).then(function(data) {
+        return next(null, randomly_return(data));
+      }).catch(function(err) {
+        throw new Error("Yelp API error: dinner");
+      });
+    }
+  };
+
+  return (new Bucks()).parallel([
+    meals_obtainer.breakfast, meals_obtainer.lunch, meals_obtainer.dinner
+  ]).add(function(err, responses, next) {
+    callback(responses);
+    return next();
+  }).end();
+};
+
 module.exports = {
+  call2: call_yelp_api,
 
 	call: function(term, location, minimum_rating, num, res) {
 
