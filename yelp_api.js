b7a0fb183f8c22491492992e6530e4b3c0b3f2e9
IzumiSy <beetle-noise@gmx.com>
2016-03-25 01:32:32 -0700

    Remove unused api v1

--- yelp_api.js
+++ yelp_api.js
@@ -5,10 +5,10 @@ var _ = require('underscore');
 var Yelp = require('yelp');
 var Bucks = require('bucks');
 var yelp = new Yelp({
-	consumer_key: config.consumer_key,
-	consumer_secret: config.consumer_secret,
-	token: config.token,
-	token_secret: config.token_secret
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  token: config.token,
+  token_secret: config.token_secret
 });
 
 var uuidBuffer = new (function() {
@@ -29,24 +29,24 @@ var uuidBuffer = new (function() {
 
 // Filter restaurants
 var filter_restaurants = function(data, minrat) {
-	var candidates = [];
+  var candidates = [];
 
-	for (business of data.businesses) {
-		if (business.rating >= minrat) {
-			var newRestaurant = new Restaurant(
-				business.name,
-				business.rating,
-				business.url,
-				business.phone,
-				business.image_url,
-				business.location.display_address,
-				business.location.coordinate
-			);
-			candidates.push(newRestaurant);
-		}
-	}
+  for (business of data.businesses) {
+    if (business.rating >= minrat) {
+      var newRestaurant = new Restaurant(
+        business.name,
+        business.rating,
+        business.url,
+        business.phone,
+        business.image_url,
+        business.location.display_address,
+        business.location.coordinate
+      );
+      candidates.push(newRestaurant);
+    }
+  }
 
-	return candidates;
+  return candidates;
 };
 
 //
@@ -62,140 +62,68 @@ var filter_restaurants = function(data, minrat) {
 // If fetching completes successfully, this function randomly selects each restaurant for every meals.
 //
 var call_yelp_api = function(query_object, location, minrat, callback) {
-	var breakfast_query = query_object.breakfast || "";
-	var lunch_query = query_object.lunch || "";
-	var dinner_query = query_object.dinner || "";
+  var breakfast_query = query_object.breakfast || "";
+  var lunch_query = query_object.lunch || "";
+  var dinner_query = query_object.dinner || "";
 
-	var randomly_return = function(data) {
-		var restaurants = filter_restaurants(data, minrat);
-		var randomly_chosen_restaurant = restaurants[_.random(0, _.size(restaurants) - 1)];
-		uuidBuffer.set_uuid_data(randomly_chosen_restaurant.uuid, randomly_chosen_restaurant);
-		return randomly_chosen_restaurant;
-	};
+  var randomly_return = function(data) {
+    var restaurants = filter_restaurants(data, minrat);
+    var randomly_chosen_restaurant = restaurants[_.random(0, _.size(restaurants) - 1)];
+    uuidBuffer.set_uuid_data(randomly_chosen_restaurant.uuid, randomly_chosen_restaurant);
+    return randomly_chosen_restaurant;
+  };
 
-	var meals_obtainer = {
-		breakfast: function(err, res, next) {
-			var query = {
-				term: "breakfast " + breakfast_query,
-				location: location
-			};
-			yelp.search(query).then(function(data) {
-				return next(null, randomly_return(data));
-			}).catch(function(err) {
-				return next(null, err);
-				//throw new Error("Yelp API error: breakfast");
-			});
-		},
-		lunch: function(err, res, next) {
-			var query = {
-				term: "lunch " + lunch_query,
-				location: location
-			}
-			yelp.search(query).then(function(data) {
-				return next(null, randomly_return(data));
-			}).catch(function(err) {
-				return next(null, err);
-				//throw new Error("Yelp API error: lunch");
-			});
-		},
-		dinner: function(err, res, next) {
-			var query = {
-				term: "dinner " + dinner_query,
-				location: location
-			}
-			yelp.search(query).then(function(data) {
-				return next(null, randomly_return(data));
-			}).catch(function(err) {
-				return next(null, err);
-				//throw new Error("Yelp API error: dinner");
-			});
-		}
-	};
+  var meals_obtainer = {
+    breakfast: function(err, res, next) {
+      var query = {
+        term: "breakfast " + breakfast_query,
+        location: location
+      };
+      yelp.search(query).then(function(data) {
+        return next(null, randomly_return(data));
+      }).catch(function(err) {
+        return next(null, err);
+        //throw new Error("Yelp API error: breakfast");
+      });
+    },
+    lunch: function(err, res, next) {
+      var query = {
+        term: "lunch " + lunch_query,
+        location: location
+      }
+      yelp.search(query).then(function(data) {
+        return next(null, randomly_return(data));
+      }).catch(function(err) {
+        return next(null, err);
+        //throw new Error("Yelp API error: lunch");
+      });
+    },
+    dinner: function(err, res, next) {
+      var query = {
+        term: "dinner " + dinner_query,
+        location: location
+      }
+      yelp.search(query).then(function(data) {
+        return next(null, randomly_return(data));
+      }).catch(function(err) {
+        return next(null, err);
+        //throw new Error("Yelp API error: dinner");
+      });
+    }
+  };
 
-	return (new Bucks()).parallel([
-		meals_obtainer.breakfast, meals_obtainer.lunch, meals_obtainer.dinner
-	]).add(function(err, responses, next) {
-		callback(responses);
-		return next();
-	}).end();
+  return (new Bucks()).parallel([
+    meals_obtainer.breakfast, meals_obtainer.lunch, meals_obtainer.dinner
+  ]).add(function(err, responses, next) {
+    callback(responses);
+    return next();
+  }).end();
 };
 
 module.exports = {
+  call_v2: call_yelp_api,
 
-	call_v2: call_yelp_api,
-
-	call_v1: function(term, location, minimum_rating, num, res) {
-
-		var candidates = [];
-		var returnValues = [];
-
-		var queryObject = {};
-		if (term != undefined) queryObject.term = term;
-		queryObject.location = location;
-
-		yelp.search(queryObject)
-			.then(function(data) {
-
-				var total_number = data.total;
-				var offset = null;
-				if (total_number <= 100) offset = 0;
-				else offset = _.random(0, 80);
-
-				queryObject.offset = offset;
-
-				yelp.search(queryObject)
-					.then(function(data) {
-
-						for (business of data.businesses) {
-
-							if (business.rating >= minimum_rating) {
-
-								var newRestaurant = new Restaurant(
-									business.name,
-									business.rating,
-									business.url,
-									business.phone,
-									business.image_url,
-									business.location.display_address,
-									business.location.coordinate
-								);
-
-								candidates.push(newRestaurant);
-							}
-						}
-
-						if (!_.isEmpty(candidates)) {
-							var random_index = _.random(0, _.size(candidates) - 1);
-							returnValue = candidates[random_index];
-						}
-
-						while (num > 0 && !_.isEmpty(candidates)) {
-							var random_index = _.random(0, _.size(candidates) - 1);
-							returnValues.push(candidates[random_index]);
-							uuidBuffer.set_uuid_data(candidates[random_index].uuid, candidates[random_index]);
-							candidates[random_index] = null;
-							num--;
-							candidates = _.compact(candidates);
-						}
-
-						res.setHeader('Content-Type', 'application/json');
-						res.send(JSON.stringify(returnValues));
-					})
-					.catch(function(err) {
-						console.error(err);
-						res.setHeader('Content-Type', 'application/json');
-						res.send(JSON.stringify(err));
-					});
-
-			})
-			.catch(function(err) {
-				console.error(err);
-				res.setHeader('Content-Type', 'application/json');
-				res.send(JSON.stringify(err));
-			});
-	},
-
-	buffer: function() {
+  buffer: function() {
     return uuidBuffer;
   }
 };
